b683ae1eb69a782a9a2f05312652d63594d135ad
Veronica <veronicagoranova@gmail.com>
2023-05-31 17:27:02 +0300

    Adding .gitignore and the very first solved problem from HackerRank.

---
+++ BasicProblems/PlusMinus/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+List<int> arr = Console.ReadLine()
+    .TrimEnd()
+    .Split(' ')
+    .ToList()
+    .Select(arrTemp => Convert.ToInt32(arrTemp))
+    .ToList();
+
+int positiveCount = 0;
+int negativeCount = 0;
+int zeroCount = 0;
+
+foreach (var element in arr)
+{
+    if (element > 0)
+    {
+        positiveCount++;
+    }
+    else if (element < 0)
+    {
+        negativeCount++;
+    }
+    else
+    {
+        zeroCount++;
+    }
+}
+
+decimal negativeRatio = (decimal)negativeCount / arr.Count;
+decimal positiveRatio = (decimal)positiveCount / arr.Count;
+decimal zeroRatio = (decimal)zeroCount / arr.Count;
+
+Console.WriteLine($"{positiveRatio:F6}");
+Console.WriteLine($"{negativeRatio:F6}");
+Console.WriteLine($"{zeroRatio:F6}");
